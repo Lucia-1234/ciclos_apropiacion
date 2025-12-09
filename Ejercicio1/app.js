63de972744605715ada4e94d681d77e348674b5f
Lucia <Lulizcano.aa@hotmail.com>
2025-12-09 17:45:43 -0500

    se finalizo el ejercicio uno haciendo uso de los ciclos con condicionales

---
+++ Ejercicio1/app.js
@@ -0,0 +1,8 @@
+//inicia ciclo for con una variable inicializada en 0
+//se indica que el ciclo se itera hasta que se deje de cumplir la condicion y el numero sea mayor a 50
+for (i = 0; i <= 50; i++) {
+  //condicion que verifica los numeros primos y los muestra en consola  
+  if (i % 2 == 0) {
+    console.log(`los numeros primos entre 1 y 50 son: ${i}`);
+  }
+}
