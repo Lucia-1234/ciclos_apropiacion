1a8a25053940e56e497c6d2fad9231fa28e34edc
Lucia <Lulizcano.aa@hotmail.com>
2025-12-09 20:01:54 -0500

    se finalizo el ejerciico haciendo uso de los ciclos do for y wile

--- Ejercicio2/app.js
+++ Ejercicio2/app.js
@@ -1,6 +1,23 @@
-let num = parseInt("Ingrese un numero");
+//se importan las librerias
+import prompt_sync from "prompt-sync";
+const prompt= prompt_sync();
 
+//se crea variable que ayuda a repetir el for d
+let continuar;
+
+//se solicita el numero 
+do{
+  let num = parseInt(prompt("Ingrese un numero: "));
+
+// inicia ciclo que se itera hasta 12 y muestra los resultados realizando la multiplicacion
 for (let i = 1; i <= 12; i++) {
-  tabla = num * i;
-  console.log(`${num} * ${i} = ${tabla}`);
+  console.log(`${num} * ${i} = ${num * i}`);
 }
+
+//se pregunta al usuario si desea continuar 
+continuar = prompt("¿Desea consultar otra tabla? (s/n):");
+
+// se lee la resppuesta y se ejecuta segun corresponda 
+} while (continuar.toLowerCase() === "s");
+
+console.log("Programa finalizado. ¡Gracias por usarlo!");
